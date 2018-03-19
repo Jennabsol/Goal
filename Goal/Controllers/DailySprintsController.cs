386a4da9eade7130bcbe5e5c222ebc21c3d4b953
jennabsol@gmail.com <jennabsol@gmail.com>
2018-03-18 23:15:21 -0500

    added sprintgroup controller and views

---
+++ Goal/Controllers/DailySprintsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Goal.Data;
+using Goal.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Goal.Controllers
+{
+    public class DailySprintsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public DailySprintsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+        //id is SprintGroupId used to add a new dailySprint to a sprint group. 
+        // GET: DailySprints
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.DailySprints.Include(d => d.SprintGroup);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: DailySprints/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dailySprints = await _context.DailySprints
+                .Include(d => d.SprintGroup)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (dailySprints == null)
+            {
+                return NotFound();
+            }
+
+            return View(dailySprints);
+        }
+
+        // GET: DailySprints/Create
+        public IActionResult Create()
+        {
+            ViewData["SprintGroupId"] = new SelectList(_context.SprintGroup, "Id", "Name");
+            return View();
+        }
+
+        // POST: DailySprints/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,DateCreated,CurrentState,Notes,SprintGroupId")] DailySprints dailySprints)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(dailySprints);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SprintGroupId"] = new SelectList(_context.SprintGroup, "Id", "Name", dailySprints.SprintGroupId);
+            return View(dailySprints);
+        }
+
+        // GET: DailySprints/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dailySprints = await _context.DailySprints.SingleOrDefaultAsync(m => m.Id == id);
+            if (dailySprints == null)
+            {
+                return NotFound();
+            }
+            ViewData["SprintGroupId"] = new SelectList(_context.SprintGroup, "Id", "Name", dailySprints.SprintGroupId);
+            return View(dailySprints);
+        }
+
+        // POST: DailySprints/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,DateCreated,CurrentState,Notes,SprintGroupId")] DailySprints dailySprints)
+        {
+            if (id != dailySprints.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(dailySprints);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DailySprintsExists(dailySprints.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SprintGroupId"] = new SelectList(_context.SprintGroup, "Id", "Name", dailySprints.SprintGroupId);
+            return View(dailySprints);
+        }
+
+        // GET: DailySprints/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dailySprints = await _context.DailySprints
+                .Include(d => d.SprintGroup)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (dailySprints == null)
+            {
+                return NotFound();
+            }
+
+            return View(dailySprints);
+        }
+
+        // POST: DailySprints/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var dailySprints = await _context.DailySprints.SingleOrDefaultAsync(m => m.Id == id);
+            _context.DailySprints.Remove(dailySprints);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DailySprintsExists(int id)
+        {
+            return _context.DailySprints.Any(e => e.Id == id);
+        }
+    }
+}
--- Goal/Controllers/SprintGroupsController.cs
+++ Goal/Controllers/SprintGroupsController.cs
@@ -24,7 +24,7 @@ namespace Goal.Controllers
         public async Task<IActionResult> Index(int id)
         {
             //return View(await _context.SprintGroup.ToListAsync());
-            //creates new instance of the DocumentGroupsList
+            //creates new instance of the ListSprintGroupViewModel
             ListSprintGroupViewModel viewModel = new ListSprintGroupViewModel();
 
 
@@ -66,17 +66,17 @@ namespace Goal.Controllers
                         dailySprints.Add(sprint);
                     }
                 }
-                //builds the viewModel with a project, list of document groups, and list of documents and returns index view with that viewModel
+                //builds the viewModel with a project, list of sprint groups, and list of dailysprints and returns index view with that viewModel
                 viewModel.SprintGroup = sprintGroup;
                 viewModel.Goal = goals;
                 viewModel.DailySprints = dailySprints;
                 return View(viewModel);
             }
 
-            //if there are no document groups for a given project ID, redirect to create a new document group 
+            //if there are no sprint groups for a given Goal ID, redirect to create a new sprint group 
             EmptyGoalsViewModel emptyGoal = new EmptyGoalsViewModel();
             emptyGoal.GoalsId = goals.Id;
-            return View("EmptyProject", emptyGoal);
+            return View("EmptyGoal", emptyGoal);
 
 
             //return View(await _context.SprintGroup.ToListAsync());
@@ -102,9 +102,16 @@ namespace Goal.Controllers
         }
 
         // GET: SprintGroups/Create
-        public IActionResult Create()
+        //id refers to Goal ID
+        public IActionResult Create(int id)
         {
-            return View();
+            //Creates a blank sprint group, but passes the Goal ID along with it
+            CreateSprintGroupViewModel viewModel = new CreateSprintGroupViewModel();
+            viewModel.GoalsId = id;
+            viewModel.SprintGroup = new SprintGroup();
+
+            return View(viewModel);
+            //return View();
         }
 
         // POST: SprintGroups/Create
@@ -112,15 +119,38 @@ namespace Goal.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,DateCreated,Completed")] SprintGroup sprintGroup)
+        public async Task<IActionResult> Create(CreateSprintGroupViewModel viewModel)
         {
+            //sets date created to current date
+            viewModel.SprintGroup.DateCreated = DateTime.Now;
             if (ModelState.IsValid)
             {
-                _context.Add(sprintGroup);
+                //sets up GoalSprintGroup and assisns GoalsId to it to update as soon as new sprint group gets created.
+                GoalSprintGroup newGoalSprintGroup = new GoalSprintGroup();
+                newGoalSprintGroup.GoalsId = viewModel.GoalsId;
+
+                //Adds new sprintgroup to DB
+                _context.Add(viewModel.SprintGroup);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+
+                //Assigns the SprintGroupId of the newly added SprintGroup to the new GoalSprintGroup model and updates the GoalSprintGroup join table
+                newGoalSprintGroup.SprintGroupId = viewModel.SprintGroup.Id;
+                _context.GoalSprintGroup.Add(newGoalSprintGroup);
+                await _context.SaveChangesAsync();
+
+                //Once added it redirects user to the index view for current goal
+                return RedirectToAction("Index", new { id = viewModel.GoalsId });
+
             }
-            return View(sprintGroup);
+            //If model validation fails will redirect back to the index view for current goal
+            return RedirectToAction("Index", new { id = viewModel.GoalsId });
+            //if (ModelState.IsValid)
+            //{
+            //    _context.Add(sprintGroup);
+            //    await _context.SaveChangesAsync();
+            //    return RedirectToAction(nameof(Index));
+            //}
+            //return View(sprintGroup);
         }
 
         // GET: SprintGroups/Edit/5
@@ -182,6 +212,7 @@ namespace Goal.Controllers
                 return NotFound();
             }
 
+            //grabs current SprintGroup to delete
             var sprintGroup = await _context.SprintGroup
                 .SingleOrDefaultAsync(m => m.Id == id);
             if (sprintGroup == null)
@@ -189,18 +220,49 @@ namespace Goal.Controllers
                 return NotFound();
             }
 
-            return View(sprintGroup);
-        }
+            //creates a new instance of DeleteSprintGroupViewModel model, that contains a sprintGroup and goal ID
+            DeleteSprintGroupViewModel viewModel = new DeleteSprintGroupViewModel();
 
+
+            //grabs goal by dipping into GoalSprintGroup join table
+            var goalSprintGroupList = await (
+                   from gsg in _context.GoalSprintGroup
+                   where gsg.SprintGroupId == sprintGroup.Id
+                   select gsg)
+                   .ToListAsync();
+
+            //grabs first return
+            var gs = goalSprintGroupList.First();
+
+            viewModel.SprintGroup = sprintGroup;
+            viewModel.GoalsId = gs.GoalsId;
+
+            //Returns delete view with viewModel
+            return View(viewModel);
+            //if (id == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //var sprintGroup = await _context.SprintGroup
+            //    .SingleOrDefaultAsync(m => m.Id == id);
+            //if (sprintGroup == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //return View(sprintGroup);
+        }
+        //id is SprintGroup.id and GoalsId is GoalsId
         // POST: SprintGroups/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> DeleteConfirmed(int id, int GoalsId)
         {
             var sprintGroup = await _context.SprintGroup.SingleOrDefaultAsync(m => m.Id == id);
             _context.SprintGroup.Remove(sprintGroup);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction("Index", new { id = GoalsId });
         }
 
         private bool SprintGroupExists(int id)
